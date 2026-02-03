4f840b7d1b97728571f44a8782fc02fa9d647334
Joel Markley <joeemarr12345@gmail.com>
2026-02-03 09:54:01 -0600

    Porting to new linux setup

--- r/politeness_eval.R
+++ r/politeness_eval.R
@@ -10,7 +10,11 @@ if (length(args) == 0) stop("No text provided")
 text <- paste(args, collapse = " ")
 
 # --- Initialize SpaCy ---
-spacy_initialize(model = "en_core_web_sm")
+# spacy_initialize(model = "en_core_web_sm")
+spacy_initialize(
+    model = "en_core_web_sm",
+    python_executable = "/home/joemar/Documents/research/argumentative_convo_mod/prosociality_evaluator/.env/bin/python"
+)
 
 # --- Politeness ---
 politeness_r <- politeness(
